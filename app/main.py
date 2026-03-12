cd6bf3fb0b808d5abfe8a647cf92a91563de4933
Pablownski <sal24374@uvg.edu.gt>
2026-03-12 16:02:03 -0600

    Creación de main, donde se encuentran los get y post para la api.

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from app.sync import sync_products
+from app.repository import (
+    list_products,
+    get_product_by_id
+)
+
+app = FastAPI("Mini Catalog")
+
+@app.post("/sync")
+def sync():
+    return sync_products()
+
+@app.get("/products")
+def get_products(limit: int = 10, offset: int = 0):
+    return list_products(limit, offset)
+
+@app.get("/products/{product_id}")
+def get_product(product_id: int):
+    product = get_product_by_id(product_id)
+    if not product:
+        return {"error": "Producto no encontrado"}
+    return product
+
